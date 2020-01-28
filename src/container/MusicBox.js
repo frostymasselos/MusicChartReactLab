b17c6b2a7405257214a090edbe4bdfff97839941
L3n1ad <danieltoth0910@gmail.com>
2020-01-28 15:18:21 +0000

    initial commit

---
+++ src/container/MusicBox.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class MusicBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <h1>I am a music box container!</h1>
+         );
+    }
+}
+ 
+export default MusicBox;
